d03bb9247ac26be19c3cd9c36686408547ca05c7
cakfadilstreamov21 <40716142+cakfadilstreamov21@users.noreply.github.com>
2018-08-01 04:44:55 +0700

    Create tess.js

---
+++ tess.js
@@ -0,0 +1,16 @@
+<script async='' src='https://cdn.onesignal.com/sdks/OneSignalSDK.js'/>
+<script>
+  var OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: &quot;67bb8ff8-651c-41b0-8a86-38b52512e87f&quot;,
+    });
+  });
+</script>
+&lt;script async src=&quot;//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js&quot;&gt;&lt;/script&gt;
+&lt;script&gt;
+     (adsbygoogle = window.adsbygoogle || []).push({
+          google_ad_client: &quot;ca-pub-5895724593526291&quot;,
+          enable_page_level_ads: true
+     });
+&lt;/script&gt;
