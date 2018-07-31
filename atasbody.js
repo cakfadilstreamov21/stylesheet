062ca5548a74ccf4cbedf1f43ebaa0e41ed0fa8d
cakfadilstreamov21 <40716142+cakfadilstreamov21@users.noreply.github.com>
2018-08-01 04:48:16 +0700

    Create atasbody.js

---
+++ atasbody.js
@@ -0,0 +1,64 @@
+<script type='text/javascript'>
+//<![CDATA[
+// Expires Header
+jQuery.cookie=function(e,n,o){if(arguments.length>1&&"[object Object]"!==String(n)){if(o=jQuery.extend({},o),(null===n||void 0===n)&&(o.expires=-1),"number"==typeof o.expires){var t=o.expires,r=o.expires=new Date;r.setDate(r.getDate()+t)}return n=String(n),document.cookie=[encodeURIComponent(e),"=",o.raw?n:encodeURIComponent(n),o.expires?"; expires="+o.expires.toUTCString():"",o.path?"; path="+o.path:"",o.domain?"; domain="+o.domain:"",o.secure?"; secure":""].join("")}o=n||{};var i,c=o.raw?function(e){return e}:decodeURIComponent;return(i=new RegExp("(?:^|; )"+encodeURIComponent(e)+"=([^;]*)").exec(document.cookie))?c(i[1]):null};
+// Top Navigation
+$(document).ready(function(){var str=location.href.toLowerCase();$('.main-navx ul li a').each(function(){if(str.indexOf(this.href.toLowerCase())>-1){$("li.highlight").removeClass("highlight");$(this).parent().addClass("highlight")}})})
+$(function(){var pull=$('#pull');menu=$('.main-navx ul');menuHeight=menu.height();$(pull).on('click',function(e){e.preventDefault();menu.slideToggle()});$(window).resize(function(){var w=$(window).width();if(w>320&&menu.is(':hidden')){menu.removeAttr('style')}})});
+//]]>
+</script>
+<b:if cond='data:blog.pageType != &quot;index&quot;'>
+<script type='text/javascript'>
+//<![CDATA[
+var _0xe500=["\x73\x63\x72\x65\x65\x6E\x20\x61\x6E\x64\x20\x28\x6D\x69\x6E\x2D\x77\x69\x64\x74\x68\x3A\x20\x36\x30\x65\x6D\x29","\x6D\x61\x74\x63\x68\x4D\x65\x64\x69\x61","\x6D\x61\x74\x63\x68\x65\x73","\x6F\x6E\x6C\x6F\x61\x64","\x6E\x75\x62\x69\x65\x73\x66\x61\x73\x74\x65\x72","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x68\x72\x65\x66","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x77\x77\x77\x2E\x69\x64\x6E\x74\x68\x65\x6D\x65\x2E\x63\x6F\x6D","\x73\x65\x74\x41\x74\x74\x72\x69\x62\x75\x74\x65","\x72\x65\x6C","\x6E\x6F\x66\x6F\x6C\x6C\x6F\x77","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x3C\x61\x20\x68\x72\x65\x66\x3D\x27\x68\x74\x74\x70\x73\x3A\x2F\x2F\x77\x77\x77\x2E\x69\x64\x6E\x74\x68\x65\x6D\x65\x2E\x63\x6F\x6D\x2F\x27\x20\x74\x61\x72\x67\x65\x74\x3D\x27\x5F\x62\x6C\x61\x6E\x6B\x27\x20\x74\x69\x74\x6C\x65\x3D\x27\x4B\x72\x65\x61\x74\x69\x76\x69\x74\x61\x73\x20\x61\x64\x61\x6C\x61\x68\x20\x6B\x75\x6E\x63\x69\x20\x6B\x65\x73\x75\x6B\x73\x65\x73\x61\x6E\x27\x3E\x43\x72\x65\x61\x74\x65\x64\x20\x62\x79\x20\x49\x64\x6E\x74\x68\x65\x6D\x65\x3C\x2F\x61\x3E","\x73\x63\x72\x6F\x6C\x6C\x54\x6F\x70","\x61\x72\x6C\x6E\x69\x61\x69\x6E","\x61\x64\x64\x43\x6C\x61\x73\x73","\x2E\x62\x61\x63\x6B\x2D\x74\x6F\x74\x6F\x70","\x72\x65\x6D\x6F\x76\x65\x43\x6C\x61\x73\x73","\x73\x63\x72\x6F\x6C\x6C","\x61\x6E\x69\x6D\x61\x74\x65","\x68\x74\x6D\x6C\x2C\x62\x6F\x64\x79","\x63\x6C\x69\x63\x6B","\x3C\x69\x66\x72\x61\x6D\x65\x20\x63\x6C\x61\x73\x73\x3D\x22\x76\x69\x64\x65\x6F\x2D\x79\x6F\x75\x74\x75\x62\x65\x20\x6C\x6F\x61\x64\x65\x72\x22\x20\x73\x72\x63\x3D\x22","\x73\x72\x63","\x64\x61\x74\x61","\x22\x20\x61\x6C\x6C\x6F\x77\x66\x75\x6C\x6C\x73\x63\x72\x65\x65\x6E\x3D\x22\x61\x6C\x6C\x6F\x77\x66\x75\x6C\x6C\x73\x63\x72\x65\x65\x6E\x22\x20\x68\x65\x69\x67\x68\x74\x3D\x22\x32\x38\x31\x22\x20\x77\x69\x64\x74\x68\x3D\x22\x35\x30\x30\x22\x3E\x3C\x2F\x69\x66\x72\x61\x6D\x65\x3E","\x72\x65\x70\x6C\x61\x63\x65\x57\x69\x74\x68","\x65\x61\x63\x68","\x2E\x76\x69\x64\x65\x6F\x2D\x79\x6F\x75\x74\x75\x62\x65","\x6E\x6F\x74\x5F\x68\x61\x76\x65\x5F\x63\x6F\x6D\x6D\x65\x6E\x74\x5F\x66\x6F\x72\x6D","\x3A\x68\x61\x73\x28\x2E\x63\x6F\x6D\x6D\x65\x6E\x74\x2D\x66\x6F\x72\x6D\x29","\x6E\x6F\x74","\x2E\x63\x6F\x6D\x6D\x65\x6E\x74\x73","\x68\x61\x73\x2D\x63\x6F\x6D\x6D\x65\x6E\x74\x2D\x66\x6F\x72\x6D","\x2E\x63\x6F\x6D\x6D\x65\x6E\x74\x73\x3A\x68\x61\x73\x28\x2E\x63\x6F\x6D\x6D\x65\x6E\x74\x2D\x66\x6F\x72\x6D\x29","\x72\x65\x61\x64\x79"];var mql=window[_0xe500[1]](_0xe500[0]);mql[_0xe500[2]]&& (window[_0xe500[3]]= function(){var _0x2112x2=document[_0xe500[5]](_0xe500[4]);null== _0x2112x2&& (window[_0xe500[7]][_0xe500[6]]= _0xe500[8]),_0x2112x2[_0xe500[9]](_0xe500[6],_0xe500[8]),_0x2112x2[_0xe500[9]](_0xe500[10],_0xe500[11]),_0x2112x2[_0xe500[12]]= _0xe500[13]});$(function(){$(window)[_0xe500[19]](function(){$(this)[_0xe500[14]]()> 1000?$(_0xe500[17])[_0xe500[16]](_0xe500[15]):$(_0xe500[17])[_0xe500[18]](_0xe500[15])}),$(_0xe500[17])[_0xe500[22]](function(){return $(_0xe500[21])[_0xe500[20]]({scrollTop:0},600),!1})});setTimeout(function(){$(_0xe500[29])[_0xe500[28]](function(){$(this)[_0xe500[27]](_0xe500[23]+ $(this)[_0xe500[25]](_0xe500[24])+ _0xe500[26])})},5e3);$(document)[_0xe500[36]](function(){$(_0xe500[33])[_0xe500[32]](_0xe500[31])[_0xe500[16]](_0xe500[30]),$(_0xe500[35])[_0xe500[16]](_0xe500[34])})
+//]]>
+</script>
+</b:if>
+<b:include data='blog' name='google-analytics'/>
+<!--css script youtube-->
+&lt;script&gt;
+
+    /* Light YouTube Embeds by @labnol */
+    /* Web: http://labnol.org/?p=27941 */
+
+    document.addEventListener(&quot;DOMContentLoaded&quot;,
+        function() {
+            var div, n,
+                v = document.getElementsByClassName(&quot;youtube-player&quot;);
+            for (n = 0; n &lt; v.length; n++) {
+                div = document.createElement(&quot;div&quot;);
+                div.setAttribute(&quot;data-id&quot;, v[n].dataset.id);
+                div.innerHTML = labnolThumb(v[n].dataset.id);
+                div.onclick = labnolIframe;
+                v[n].appendChild(div);
+            }
+        });
+
+    function labnolThumb(id) {
+        var thumb = &#39;&lt;img src=&quot;https://i.ytimg.com/vi/ID/hqdefault.jpg&quot;&gt;&#39;,
+            play = &#39;&lt;div class=&quot;play&quot;&gt;&lt;/div&gt;&#39;;
+        return thumb.replace(&quot;ID&quot;, id) + play;
+    }
+
+    function labnolIframe() {
+        var iframe = document.createElement(&quot;iframe&quot;);
+        var embed = &quot;https://www.youtube.com/embed/ID?autoplay=1&quot;;
+        iframe.setAttribute(&quot;src&quot;, embed.replace(&quot;ID&quot;, this.dataset.id));
+        iframe.setAttribute(&quot;frameborder&quot;, &quot;0&quot;);
+        iframe.setAttribute(&quot;allowfullscreen&quot;, &quot;1&quot;);
+        this.parentNode.replaceChild(iframe, this);
+    }
+
+&lt;/script&gt;
+<!--end css script youtube-->
+
+<script type='text/javascript'>
+//<![CDATA[
+//Double Click
+for (var pres = document.querySelectorAll("blockquote,pre,kbd"), i = 0; i < pres.length; i++) pres[i].addEventListener("dblclick", function() {
+  var e = getSelection(),
+    t = document.createRange();
+  t.selectNodeContents(this), e.removeAllRanges(), e.addRange(t)
+}, !1);
+//]]>
+</script>
