4a12e2c861eb2de365397e57d6d56d24b8e7da3a
cakfadilstreamov21 <40716142+cakfadilstreamov21@users.noreply.github.com>
2018-08-01 04:19:37 +0700

    Create tes.js

---
+++ tes.js
@@ -0,0 +1,23 @@
+<style>
+table {
+    width:100%;
+}
+table, th, td {
+    border: 1px solid white;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 0px 5px 0px 5px;
+    text-align: left;
+}
+table#t01 tr:nth-child(even) {
+    background-color: #eee;
+}
+table#t01 tr:nth-child(odd) {
+   background-color: #fff;
+}
+table#t01 th {
+    background-color: red;
+    color: white;
+}
+</style>
